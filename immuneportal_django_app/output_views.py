6a140bc416c41c87bcccf5c77f6932caa4dd9a4e
jkaefer <kaeferj1@gmail.com>
2021-05-03 17:43:35 -0400

    latest

--- immuneportal_django_app/output_views.py
+++ immuneportal_django_app/output_views.py
@@ -115,7 +115,7 @@ class irneoSeq:
 		#link = urls.get_download_url(experiment_output.value)
 		#print("FILE URL",link)
 		#print('directory',os.getcwd())
-		df=pandas.read_csv(output_file.read().decode())
+		df=pandas.read_csv(output_file.read().decode(),delimiter='\t')
 		#print(dfTest.head())
 		#print('contents',os.listdir('django_airavata/static'))
 		

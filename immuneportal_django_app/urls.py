e5318a405abdb53fd99045bb3aba7398ce13fc44
jkaefer <kaeferj1@gmail.com>
2021-06-17 11:07:03 -0400

    removed problematic view

--- immuneportal_django_app/urls.py
+++ immuneportal_django_app/urls.py
@@ -7,7 +7,7 @@ app_name = 'immuneportal_django_app'
 urlpatterns = [
     url(r'^expviz/', views.expviz, name="expviz"),
     url(r'^image_view/', views.image_view, name="image_view"),
-    url(r'^upset_view/', views.upset_view, name="upset_view"),
+    #url(r'^upset_view/', views.upset_view, name="upset_view"),
     url(r'^splicetable/', views.splice_res, name="splice"),
     #url(r'^languages/', views.languages, name="languages"),
 ] 

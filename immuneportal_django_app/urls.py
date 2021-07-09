84c52a7fe6ef05f500437de191dddfe49d4c8f7a
jkaefer <kaeferj1@gmail.com>
2021-07-09 03:02:46 -0400

    assumedWorking

--- immuneportal_django_app/urls.py
+++ immuneportal_django_app/urls.py
@@ -7,7 +7,10 @@ app_name = 'immuneportal_django_app'
 urlpatterns = [
     url(r'^expviz/', views.expviz, name="expviz"),
     url(r'^image_view/', views.image_view, name="image_view"),
-    
+    #url(r'^both/', views.both, name="both"),
+    url(r'^upset_view/', views.upset_view, name="upset_view"),
+    #url(r'^hlaload_view/', views.hlaload_view, name="hlaload_view"),
+    #url(r'^delete/', views.delete, name="delete"),
     url(r'^splicetable/', views.splice_res, name="splice"),
     #url(r'^languages/', views.languages, name="languages"),
 ] 
--- immuneportal_django_app/views.py
+++ immuneportal_django_app/views.py
@@ -90,8 +90,143 @@ def image_view(request):
 	return HttpResponse(image_bytes, content_type="image/png")
 	
 
+@login_required
+def upset_view(request):
+	currkey=None
+	for key in list(request.GET.keys()):
+		print(key)
+		if 'airavata-dp' in key:
+			currkey=key
+			
+			#currkey=key.replace("thisuri,","");
+			print(currkey)
+	#if 'upset' in list(request.GET.keys()):
+	#print('UPSET VIEW CURRENT DIRECTORY',os.getcwd())
+	fdata=user_storage.open_file(request, data_product=None, data_product_uri=currkey)
+	d1=pd.read_csv(fdata,header=0,sep="\t")
+	print('SUPPOSED SUCCESS')
+	hla_cols = [col for col in d1.columns if 'HLA-' in col]
+	d2= d1[hla_cols]
+	dlist= d2.values.tolist()
 
 
+	upset_list=[]
+
+	for item in dlist:
+		indexs= [ n for n,i in enumerate(item) if i<2 ]
+		hlagroups= ','.join([ hla_cols[j] for j in indexs])
+		upset_list.append(hlagroups)
+
+	sumList= [ hla_cols + ['count'] ]
+	cc= Counter(upset_list)
+	for key, value in cc.items():
+		hlaS= key.split(',')
+		ToF=[]
+		for m in hla_cols:
+			if m in hlaS:
+				ToF.append(True)
+			else:
+				ToF.append(False)
+		
+		sumList.append(ToF + [value])
+
+	upSet= pd.DataFrame(sumList[1:],columns=sumList[0]);
+	print(upSet) #### here for the right panel data
+	counts=upSet['count']
+	upSet = upSet.drop(labels='count', axis=1)
+
+	#INITIALIZE TUPLE INDEX TO GET RID OF DUPLICATES
+	tupList=[]
+	for idx, row in upSet.iterrows():
+
+		tupList.append(tuple(row))
+	index = pd.MultiIndex.from_tuples(tupList, names=list(upSet.columns))
+	upsetData=pd.Series(numpy.array(list(counts)), index=index)
+	
+	plot(upsetData)
+
+	
+	buffer = io.BytesIO()
+	plt.savefig(buffer, format='png')
+	image_bytes2 = buffer.getvalue()
+	#savedFile1=user_storage.save(request,'.',image_bytes2,name='upset', content_type=None, storage_resource_id=None)
+	#print("SAVED FILE",savedFile1)
+	image_bytes2=base64.b64encode(image_bytes2)
+	buffer.close()
+	#time.sleep(1)
+	
+	
+	return HttpResponse(image_bytes2, content_type="image/png")
+	
+	
+'''
+@login_required
+def hlaload_view(request):
+	print("THESE KEYS",request.GET.keys())
+	
+	
+	currkey=None
+	for key in list(request.GET.keys()):
+		print(key)
+		if 'airavata-dp' in key:
+			currkey=key
+			
+			#currkey=key.replace("thisuri,","");
+			print(currkey)
+	#if 'upset' in list(request.GET.keys()):
+	#print('UPSET VIEW CURRENT DIRECTORY',os.getcwd())
+	fdata=user_storage.open_file(request, data_product=None, data_product_uri=currkey)
+	
+	print('SUPPOSED SUCCESS')
+	
+	d1=pd.read_csv(fdata,header=0,sep="\t")
+	individual=len(d1)
+	#individual=int(str(check_output(["wc","-l","django_airavata/static/files/Immunoportal_data.txt"]).decode("utf-8")).split(' ')[0])
+	tcga=pandas.read_csv('TCGA_IRneoAg_load_in_8cancers_summary.csv')
+
+	tcga=tcga[tcga['canType']=='BRCA']
+	print(tcga)
+
+	#alpha, loc , beta=5,100,22
+	#data=ss.gamma.rvs(alpha,loc=loc,scale=beta,size=5000)
+
+	plt.axvline(individual, color='red')
+
+	ax=sns.kdeplot(tcga['weakBinder'])
+	plt.xlabel("Intron Neoantigen Load")
+
+	buffer = io.BytesIO()
+	
+	#savedFile=user_storage.save(request,'.',image_bytes,name=None, content_type=None, storage_resource_id=None)
+	
+	plt.savefig(buffer, format='png')
+	image_bytes = buffer.getvalue()
+	savedFile2=user_storage.save(request,'.',image_bytes,name=None, content_type=None, storage_resource_id=None)
+	print("SAVED FILE",savedFile2)
+	#image_bytes=base64.b64encode(image_bytes)
+	buffer.close()
+	#time.sleep(1)
+	
+	return HttpResponse(savedFile.productUri, content_type="text/html")
+	
+@login_required
+def delete(request):
+	print("THESE KEYS",request.GET.keys())
+	
+	
+	currkey=None
+	for key in list(request.GET.keys()):
+		print(key)
+		if 'airavata-dp' in key:
+			currkey=key
+			
+			#currkey=key.replace("thisuri,","");
+			print(currkey)
+	user_storage.delete(request,data_product=None, data_product_uri=currkey)
+	
+	return HttpResponse('deleted', content_type="text/html")
+
+'''
 @login_required
 def expviz(request):
 	return render(request, "immuneportal_django_app/expviz.html")

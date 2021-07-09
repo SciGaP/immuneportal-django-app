e4eba689dd7ada5ab72861fb0c04113a33d4a18b
jkaefer <kaeferj1@gmail.com>
2021-07-09 03:22:32 -0400

    click rows

--- immuneportal_django_app/urls.py
+++ immuneportal_django_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     #url(r'^both/', views.both, name="both"),
     url(r'^upset_view/', views.upset_view, name="upset_view"),
     #url(r'^hlaload_view/', views.hlaload_view, name="hlaload_view"),
-    #url(r'^delete/', views.delete, name="delete"),
+    #git url(r'^delete/', views.delete, name="delete"),
     url(r'^splicetable/', views.splice_res, name="splice"),
     #url(r'^languages/', views.languages, name="languages"),
 ] 
